e4904a0816063e1948c38f06c244007096584486
Rapilit <galileog985@gmail.com>
2026-01-10 23:55:06 +0700

    proxy api

--- frontend/frontend/next.config.ts
+++ frontend/frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://151.243.222.93:30550/api/:path*',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
